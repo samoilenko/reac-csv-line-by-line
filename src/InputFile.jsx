942443b4cdf2600a47a9ca0cb7b0d71b9f8ded1d
Sergey Samoilenko <s.samoilenko@gmail.com>
2020-10-20 09:55:33 +0300

    wrap onChange callback into useCallback

--- src/InputFile.jsx
+++ src/InputFile.jsx
@@ -1,14 +1,14 @@
-import React, { useRef } from 'react';
+import React, { useRef, useCallback } from 'react';
 import T from 'prop-types';
 
 const InputFile = ({ onFileSelect }) => {
     const fileInput = useRef();
 
-    const onChange = () => {
+    const onChange = useCallback(() => {
         const reader = new FileReader();
         reader.onload = e => { onFileSelect(e.target.result.split('\n')); };
         reader.readAsText(fileInput.current.files[0]);
-    };
+    }, [onFileSelect]);
 
     return <input type="file" onChange={onChange} ref={fileInput} />
 };
